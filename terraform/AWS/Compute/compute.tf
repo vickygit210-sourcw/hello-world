d7f67946cc4bc979e587b0cbae8df3a61bede068
vicky <vicky210@gmail.com>
2022-07-15 16:16:00 +0530

    added key cicd

--- terraform/AWS/Compute/compute.tf
+++ terraform/AWS/Compute/compute.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance"{
   count = var.instancecount
   ami = var.amiid
   instance_type = var.instancetype
+  key_name = "cicd"
   subnet_id = var.subnetid[count.index % length(var.subnetid)]
   vpc_security_group_ids = tolist([var.securitygroup])
 
