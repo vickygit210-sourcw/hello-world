7ab242122afc1fbe0eede111beab7525d218ba6c
vickygit210-sourcw <71427460+vickygit210-sourcw@users.noreply.github.com>
2022-07-19 21:45:13 +0530

    Update compute.tf
    Key updated to jenkinspoc

--- terraform/AWS/Compute/compute.tf
+++ terraform/AWS/Compute/compute.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance"{
   count = var.instancecount
   ami = var.amiid
   instance_type = var.instancetype
-  key_name = "docker"
+  key_name = "jenkinspoc"
   subnet_id = var.subnetid[count.index % length(var.subnetid)]
   vpc_security_group_ids = tolist([var.securitygroup])
 
