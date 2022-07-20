22596bdab50994912532b2f9dde69464c956fab5
vicky <vicky210@gmail.com>
2022-07-20 10:03:18 +0530

    Added default egress rule to devopssg

--- terraform/AWS/Network/securitygroup/sg.tf
+++ terraform/AWS/Network/securitygroup/sg.tf
@@ -13,6 +13,13 @@ resource aws_security_group sg{
       protocol = ingress.value.protocol
       cidr_blocks = ingress.value.cidrblock
     }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   }
 }
